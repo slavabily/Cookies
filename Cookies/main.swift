54c5c9711ce265cc0ebf747239a6c45de17d99c6
slava bily <slavabily@me.com>
2021-12-18 15:48:27 +0200

    the flow for finding two minimum array members less then k each is implemented

--- Cookies/main.swift
+++ Cookies/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
+func cookies(k: Int, A: [Int]) -> Int {
+    var arr = A.sorted()
+    var iter = 0
+    while arr.count > 1 {
+        var minArr = [Int]()
+        while minArr.count < 2 {
+            if let min = arr.min() {
+                if min < k {
+                    arr.remove(at: arr.firstIndex(of: min)!)
+                    minArr.append(min)
+                } else {
+                    return -1
+                }
+            }
+        }
+        print(minArr)
+    }
+    return -1
+}
+
+print(cookies(k: 6, A: [1, 2, 3, 9, 10, 12]))
 
