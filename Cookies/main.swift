dbb6bdf0151837c98a56e5d720ff770220498008
slava bily <slavabily@me.com>
2021-12-18 19:38:32 +0200

    draft of algorithm for array optimisation is created

--- Cookies/main.swift
+++ Cookies/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 func cookies(k: Int, A: [Int]) -> Int {
-    var arr = A.sorted()
+    var arr = A
     var iter = 0
     while arr.count > 1 {
         var minArr = [Int]()
@@ -18,14 +18,18 @@ func cookies(k: Int, A: [Int]) -> Int {
                     arr.remove(at: arr.firstIndex(of: min)!)
                     minArr.append(min)
                 } else {
-                    return -1
+                    return iter
                 }
             }
         }
         print(minArr)
+        let incrMin = minArr[0] + 2 * minArr[1]
+        arr.insert(incrMin, at: 0)
+        print(arr)
+        iter += 1
     }
     return -1
 }
 
-print(cookies(k: 6, A: [1, 2, 3, 9, 10, 12]))
+print(cookies(k: 7, A: [1, 2, 3, 9, 10, 12]))
 
